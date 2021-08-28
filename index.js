7fe65d9da2772fceb2536624ca9faafa48767c7d
mamun <abdullah.ssc13@gmail.com>
2021-08-28 11:13:40 +0600

    testing for digitalit

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+
+    const accounts = [{userName: 'mamun007', password: 'mamun.me.417'}, {userName: 'mamun009', password: '121212'}];
+
+    // for (const acc of accounts) {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto('https://app.slack.com/workspace-signin', {waitUntil: 'networkidle2', timeout: 0});
+    await page.goto('https://digitalit.app/', {waitUntil: 'networkidle2', timeout: 0});
 //await page.waitForNavigation({waitUntil: 'networkidle2'})
-//await page.screenshot({path: 'example.png'});
-    await page.type('input[name=domain]', 'exonhost')
-//await page.type('input[name=q]', 'noman')
+    await page.type('input[name=username]', 'mamun007')
+    await page.type('input[name=password]', 'mamun.me.417')
+    // await page.screenshot({path: 'example.png'});
+
     await page.keyboard.press('Enter');
     await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 0})
-    await page.type('input[name=email]', 'abdullah.ssc13@gmail.com')
-    await page.type('input[name=password]', 'mamun017508')
-    await page.keyboard.press('Enter');
+    await page.goto('https://digitalit.app/hourly-income', {waitUntil: 'networkidle2', timeout: 0});
+    await page.goto('https://digitalit.app/logout', {waitUntil: 'networkidle2', timeout: 0});
 
-    try {
-        await page.waitForNavigation({waitUntil: 'networkidle2'})
-        console.log('try');
-    } catch {
-        console.log('catch');
-    }
+//     }    console.log('ia');
+//     await page.click('div.c-texty_input__container.c-texty_input__container--multi_line');
+//     console.log('last');
+// // await page.keyboard.press('ArrowLeft');
+//     await page.keyboard.type('/atlog in');
+// //await page.keyboard.press('Enter');
+// // await page.mouse.down();
+// // await page.mouse.move(345, 680);
+// // await page.mouse.up();
 
-    console.log('ia');
-    await page.click('div.c-texty_input__container.c-texty_input__container--multi_line');
-    console.log('last');
-// await page.keyboard.press('ArrowLeft');
-    await page.keyboard.type('/atlog in');
-//await page.keyboard.press('Enter');
-// await page.mouse.down();
-// await page.mouse.move(345, 680);
-// await page.mouse.up();
+
 })();
